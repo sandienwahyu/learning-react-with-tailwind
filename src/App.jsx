699001ba4b27afa5d353d759f4a57c5c3f6da523
sandienwahyu <sandienwahyu@gmail.com>
2026-04-15 17:42:03 +0700

    3-components-and-props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import "./App.css";
 
+function Button({ color, colorHover, children = "Button" }) {
+  return (
+    <button
+      className={`border-purple-200 ${color} text-purple-600 hover:border-transparent hover:${colorHover} hover:text-white active:bg-purple-700 rounded-4xl px-2 py-1`}
+    >
+      {children}
+    </button>
+  );
+}
+
 export default function App() {
   return (
-    <>
-      <div className="flex justify-center items-center bg-blue-600 min-h-screen">
-        <h1 className="text-3xl font-bold underline">Hello World</h1>
-      </div>
-    </>
+    <div className="flex justify-center items-center bg-blue-600 min-h-screen gap-2">
+      <Button color={"bg-white"} colorHover={"bg-purple-600"}>
+        Message
+      </Button>
+      <Button
+        color={"bg-black"}
+        colorHover={"bg-slate-600"}
+        children={"Submit"}
+      />
+      <Button color={"bg-slate-600"} colorHover={"bg-white"} />
+    </div>
   );
 }
